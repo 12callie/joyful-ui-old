3d1d579d55933fb6385cf9eaffdedd5c7ae289cf
collie <2541474139@qq.com>
2023-03-03 19:26:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <TopNav />
+  <a href="#">1</a>
+  <h2>111</h2>
+</template>
+
+<script setup>
+import TopNav from "./components/TopNav.vue";
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ s }}
+    <button @click="add">+1</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const s = ref<number>(1);
+function add() {
+  s.value += 1;
+}
+</script>
+
+
+<style scoped>
+</style>
